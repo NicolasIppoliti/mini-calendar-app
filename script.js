7dd57f6baea28ba2ab99d4ac4dd9bc5559aa5467
NicolasIppoliti <contacto.nicolasippoliti@gmail.com>
2024-03-06 20:48:45 -0300

    v1.0

---
+++ script.js
@@ -0,0 +1,14 @@
+const date = document.getElementById('date');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+
+const today = new Date();
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+date.textContent = (today.getDate() < 10 ? '0' : '') + today.getDate();
+day.textContent = days[today.getDay()];
+month.textContent = months[today.getMonth()];
+year.textContent = today.getFullYear();
